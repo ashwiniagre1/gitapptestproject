178943dbbaf61a69f7a34b51e0b4c1b4aee4f196
ashwiniagre1 <31474358+ashwiniagre1@users.noreply.github.com>
2018-08-29 17:26:34 +0530

    pull test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  let testpullresponse:any;
  constructor(private _router: Router){
 
  }
